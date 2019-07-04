0097a1e892b2d591ecbc47652357c4e9b705322d
Anushka Panagar <apanagar@dufresne.ca>
2019-07-04 15:23:28 -0500

    Second COmmit

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,216 @@
+﻿using Sudoku;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    public class Sudoku
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void Main()
+        {
+            int i, j;
+            int[,] arr1 = new int[9, 9];
+            Console.WriteLine("Read a 9x9 2D array");
+            Console.WriteLine("Input Matrix Element");
+            for (i = 0; i < 9; i++)
+            {
+                for (j = 0; j < 9; j++)
+                {
+                    Console.Write("element - [{0},{1}] : ", i, j);
+                    try
+                    {
+                        arr1[i, j] = Convert.ToInt32(Console.ReadLine());
+                        if (arr1[i, j] > 9 || arr1[i, j] < 0)
+                        {
+                            Console.WriteLine("element [{0},{1}] out of allowed range (1-9)");
+                        }
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("element - [{0},{1}] is not an integer", i, j);
+                    }
+                }
+            }
+          //  if (Sudoku.CheckRow(arr1) && Sudoku.CheckColumn(arr1) && Sudoku.CheckMatrix(arr1))
+            if (Sudoku.CheckRow(arr1))
+            {
+                Console.WriteLine("Sudoku is Valid - TRUE");
+            }else
+            {
+                Console.WriteLine("Sudoku is Not Valid - FALSE");
+            }
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="arr1"></param>
+        /// <returns></returns>
+        public static bool CheckRow(int[,] arr1)
+        {
+            bool result = false;
+            try
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    var row = Enumerable.Range(0, 9).Select(x => arr1[i, x]);
+                    if (row.Distinct().Count() != 9)
+                    {
+                        result = false;
+                        break;
+                    }
+                    result = true;
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred: " + ex.Message);
+                throw ex;
+            }
+        }
+        public static bool CheckColumn(int[,] arr1)
+        {
+            bool result = false;
+            try
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    var column = Enumerable.Range(0, 9).Select(x => arr1[x, i]);
+                    if (column.Distinct().Count() != 9)
+                    {
+                        result = false;
+                        break;
+                    }
+                    result = true;
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred: " + ex.Message);
+                throw ex;
+            }
+        }
+
+
+
+
+
+
+
+
+
+        #region
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="arr1"></param>
+        /// <returns></returns>
+        //public static bool CheckColumn(int[,] arr1)
+        //{
+        //    bool result = false;
+        //    try
+        //    {
+        //        for (int i = 0; i < arr1.GetLength(0); i++)
+        //        {
+        //            for (int j = 0; j < arr1.GetLength(1); j++)
+        //            {
+        //                if (j == 0)
+        //                {
+        //                    result = true;
+        //                }
+        //                else if (arr1[j, i] == arr1[j - 1, i])
+        //                {
+        //                    result = false;
+        //                }
+        //            }
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine("Error occurred: " + ex.Message);
+        //        throw ex;
+        //    }
+        //    return result;
+        //}
+        #endregion
+
+        #region
+        
+        public static bool CheckMatrix(int[,] arr1)
+        {
+          
+           
+
+            for (int i = 0; i < arr1.GetLength(0); i++)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    
+
+                }
+            }
+            return false;
+
+           
+
+        
+        }
+        #endregion
+
+        public static bool testRegion(int[,] arr1, int startRow, int startCol)
+        {
+            int[] tmp = new int[arr1.Length];
+            int index = 0;
+            for(int i = startRow; i < startRow + 3; i++)
+            {
+                for (int j = startCol; j < startCol + 3; j++)
+                {
+                    tmp[index] = arr1[i,j];
+                    index++;
+                    
+                }
+            }
+            Console.WriteLine(tmp);
+            return false;
+        }
+    }
+}
+
+//  return false;
+//var number_count = new Dictionary<int, int>();
+//for (int i = 0; i < 3 ; i++)
+//{
+//    for (int j = 0; j < 3; j++)
+//    {
+//        var number = arr1[i, j];
+//        if (!number_count.ContainsKey(number))
+//        {
+//            number_count[number] = 0;
+//        }
+//        number_count[number] += 1;
+//       // Console.WriteLine(number);
+//    }
+
+//}
+//foreach (var key in number_count)
+//{
+//    if (key.Value > 1)
+//    {
+//        result = true;
+//    }
+//    else
+//    {
+//        result = false;
+//    }
+//}
---
+++ UnitTestProject4/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject4
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        int[,] arr1 = { {5,3,4,6,7,8,9,1,2 }, {6,7,2,1,9,5,3,4,8}, {1,9,8,3,4,2,5,6,7}, {8,5,9,7,6,1,4,2,3}, {4,2,6,8,5,3,7,9,1}, {7,1,3,9,2,4,8,5,6} , {9,6,1,5,3,7,2,8,4}, {2,8,7,4,1,9,6,3,5}, {3,4,5,2,8,6,1,7,9} };
+
+        [TestMethod]
+        public void CheckRowTest()
+        {
+            try
+            {
+               bool expected = true;
+                Sudoku.Sudoku.CheckRow(arr1);               
+               bool actual = Sudoku.Sudoku.CheckRow(arr1);
+                Assert.AreEqual(expected, actual, "true" ,"Test Passed");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error occurred: " + ex.Message);
+                throw ex;
+            }
+          
+        }
+
+        [TestMethod]
+        public void CheckColumnTest()
+        {
+            try
+            {
+                bool expected = true;
+                Sudoku.Sudoku.CheckColumn(arr1);
+                bool actual = Sudoku.Sudoku.CheckColumn(arr1);
+                Assert.AreEqual(expected, actual, "true", "Test Passed");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred: " + ex.Message);
+                throw ex;
+            }
+        }
+
+        [TestMethod]
+        public void CheckMatrixTest()
+        {
+            try
+            {
+                bool expected = true;
+                Sudoku.Sudoku.CheckMatrix(arr1);
+                bool actual = Sudoku.Sudoku.CheckMatrix(arr1);
+                Assert.AreEqual(expected, actual, "true", "Test Passed");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred: " + ex.Message);
+                throw ex;
+            }
+        }
+    }
+}
